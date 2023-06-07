f233c489a7183e8b231a6d14c2947801f9c52d1f
Thanh Nguyen <thanh.nguyen1@tomosia.com>
2023-06-07 13:16:11 +0700

    Initial Commit

---
+++ EditorTextUI/EditorTextUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditorTextUIApp.swift
+//  EditorTextUI
+//
+//  Created by kii on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct EditorTextUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
